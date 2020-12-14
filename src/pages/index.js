1c00cfa56ad8d9f6ed0ebe419bd757237650a504
blkclct <paradox.h129@gmail.com>
2020-12-15 02:31:10 +0900

    :tada: Add: ダークモード対応追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const handleToggle = e => {
+    setIsDarkTheme(e.target.checked);
+    if (isDarkTheme) {
+      document.documentElement.removeAttribute("data-theme");
+    } else document.documentElement.setAttribute("data-theme", "dark");
+  };
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <label>
+        <input
+          type="checkbox"
+          checked={isDarkTheme}
+          onChange={handleToggle}
+          style={{ display: `none` }}
+        />
+        {isDarkTheme ? '🌚' :　'🌞'}
+      </label>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
